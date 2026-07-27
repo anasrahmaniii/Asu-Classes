fddd4fe70bb48cec60a540c0b93641f71c595305
anasrahmaniii <xanasrahmani@gmail.com>
2026-07-27 14:03:32 +0530

    Delete verify2.cjs

--- verify2.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('admin.html', 'utf8');
-
-const closeModal = html.substring(html.indexOf('function closeModal()'), html.indexOf('let editingCourseId = null;'));
-console.log(closeModal);
